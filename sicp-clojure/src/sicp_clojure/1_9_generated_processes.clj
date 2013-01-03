5332b303d0354a4ca07271677296d801acc12742
Daniel Dyba <daniel.dyba@gmail.com>
2013-01-03 11:56:24 -0800

    Provided explanation for 1.9 solution
    * Can't port Scheme implementation of 1.9's recursive function
      to Clojure because of the limitations of the JVM, see recur

--- sicp-clojure/src/sicp_clojure/1_9_generated_processes.clj
+++ sicp-clojure/src/sicp_clojure/1_9_generated_processes.clj
@@ -9,3 +9,17 @@
     (if (= x 0)
       y
       (recur (dec x) (inc y)))))
+
+;; The following scheme function can't be ported to Clojure
+;;
+;; (define (+ a b)
+;;   (if (= a 0)
+;;       b
+;;       (inc (+ (dec a ) b))))
+;;
+;; The reason is because in Clojure, you need to have the
+;; recursion point be at the tail end of the function. Here
+;; inc is the tail end of the function and we call the +
+;; function inside the argument of the inc function. We
+;; would not be able to do this in Clojure: we can't call
+;; recur inside the inc function.
