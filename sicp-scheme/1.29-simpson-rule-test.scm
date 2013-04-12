e49ee011b032abc50359686caec0ba8329cccaf3
mach10 <rob.c.shield@googlemail.com>
2013-04-12 19:18:55 +0100

    attempt to finish Simpson

--- sicp-scheme/1.29-simpson-rule-test.scm
+++ sicp-scheme/1.29-simpson-rule-test.scm
@@ -7,6 +7,9 @@
 (check-equal? (h 0 1 100) 1/100 "it should give me 1/100 back")
 (check-equal? (h 2 4 1) 2 "it should give me 2 back")
 
-(check-equal? (yk cube 2 4 1) (cube 4) "it should give me 2 back")
+(check-equal? (y cube 2 4 2) (cube 4) "it should give me 2 back")
 
-(check-equal? (round (* 100000000 (bart cube 0 1 n))) .249999875 "it should be more precise than the previous method")
+;;(check-equal? (round (* 100000000 (simpson cube 0 1 n))) .249999875 "it should be more precise than the previous method")
+(check-equal? (simpson cube 0 1 1000) 0.9996666666666666)
+(check-equal? (integral cube 0 1 0.01) 0.24998750000000042)
+;;(check-equal? (integral cube 0 1 1000) 0.24998750000000042)
--- sicp-scheme/1.29-simpson-rule.scm
+++ sicp-scheme/1.29-simpson-rule.scm
@@ -2,20 +2,36 @@
 
 (define (h a b n) (/ (- b a) n))
 
-(define (yk f a b n) 
-  (f (+ a (* n (h a b n)))))
-
-(define (bart f a b n)
-  (if (= n 0)
-    0
+(define (y f a b n) 
+  (if (= n 0) 0
+  (f (+ a (* n (h a b n))))))
+;; cube 0 1 100
+(define (bart f a b ncurrent nlimit)
+  (if (= ncurrent nlimit)
+     (+ (y f a b ncurrent) 0 )
     (+ (if 
-         (even? n) 
-         (* 2 (yk f a b n)) 
-         (* 4 (yk f a b n))) 
-       (bart f a b (- n 1)))))
+         (even? ncurrent) 
+         (* 2.0 (y f a b ncurrent)) 
+         (* 4.0 (y f a b ncurrent))) 
+       (bart f a b (+ ncurrent 1.0) nlimit))))
 
-(provide bart h yk)
+
+(define( simpson f a b n)
+  (* (/ (h a b n ) 3) (bart f a b 1 n))
+  )
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (integral f a b dx)
+  (define (add-dx x)
+    (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b) dx))
+(provide integral simpson h y)
 
 ;;(/ (* h (y0 +4y1 +2y2 +4y3 +2y4 +...+2yn−2 +4yn−1 + yn)) 3)
 ;;where h=(b−a)/n
 ;;and yk =f(a+kh)
+;; so y0 = cube(a+0h)
