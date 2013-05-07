b019fdef724615b721f580355bb1526559b3b3d7
reborg <reborg@reborg.net>
2013-05-07 17:08:03 +0100

    removing those println

--- sicp-clojure/src/sicp_clojure/1_14_coin_change_growth.clj
+++ sicp-clojure/src/sicp_clojure/1_14_coin_change_growth.clj
@@ -10,18 +10,12 @@
 (defn cc [amount kinds-of-coins identation]
   (do 
     (cond (= amount 0)
-            (do (println (str (apply str (repeat identation "-")) "> (cc " amount " " kinds-of-coins " " identation ") returning 1"))
-              1)
+          1
           (or (< amount 0) (= kinds-of-coins 0))
-            (do (println (str (apply str (repeat identation "-")) "> (cc " amount " " kinds-of-coins " " identation ") returning 0"))
-              0)
+          0
           :else (do
-                  (println (str (apply str (repeat identation "-")) "> (cc " amount " " kinds-of-coins " " identation ")"))
-                  (println (str ";(+ " 
-                                "(cc " amount " " (- kinds-of-coins 1) " " (inc identation) ") "
-                                "(cc " (- amount (first-denomination kinds-of-coins)) " " kinds-of-coins " " (inc identation) "))"))
                   (+ (cc amount (- kinds-of-coins 1) (inc identation)) 
-                   (cc (- amount (first-denomination kinds-of-coins)) kinds-of-coins (inc identation)))))))
+                     (cc (- amount (first-denomination kinds-of-coins)) kinds-of-coins (inc identation)))))))
 
 (defn count-change [amount]
   (cc amount 5 0))
