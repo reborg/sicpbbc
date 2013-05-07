2ae460567e7b017ee12282505944eb1ccdcd6613
reborg <reborg@reborg.net>
2013-05-07 17:08:21 +0100

    moved to latest clojure and midje

--- sicp-clojure/project.clj
+++ sicp-clojure/project.clj
@@ -3,9 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/math.numeric-tower "0.0.2"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0"]
+  :profiles {:dev {:dependencies [[midje "1.5.1"]
                                   [com.stuartsierra/lazytest "1.2.3"]]
-                   :repositories {"stuart" "http://stuartsierra.com/maven2"}}}
-  :plugins [[lein-midje "2.0.0-SNAPSHOT"]])
+                   :repositories {"stuart" "http://stuartsierra.com/maven2"}}})
