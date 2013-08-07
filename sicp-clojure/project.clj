28b95e79b0e2996eee1a7a98953665d060e3b39c
reborg <reborg@reborg.net>
2013-08-07 11:20:37 +0100

    Added lein midje plugin.

--- sicp-clojure/project.clj
+++ sicp-clojure/project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/math.numeric-tower "0.0.2"]]
+  :plugins [[lein-midje "3.1.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]
                                   [com.stuartsierra/lazytest "1.2.3"]]
                    :repositories {"stuart" "http://stuartsierra.com/maven2"}}})
